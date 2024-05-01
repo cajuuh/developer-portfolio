151f062549614a40f6fe02ae784a4bc969651372
Pedro Alcântara <pedro.filipi.alcantara@gmail.com>
2024-05-01 13:25:43 -0300

    add e-mail service config

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Pedro Alcântara",
   profile: "/profile.png",
-  designation: "Software Developer",
+  designation: "Software Engineer",
   description:
     "My name is Pedro Alcântara. I am a Senior Software Engineer with over 7 years of experience in the industry. My passion lies in front-end development, in the JS ecosystem, specializing in ReactJS and React-Native. I'm recognized for my ability to produce clean code and consistently meet project deadlines, reflecting his dedication to high-quality work.",
   email: "pedro.filipi.alcantara@gmail.com",
